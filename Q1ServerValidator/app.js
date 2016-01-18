93ce2e36afc79249bb8cca21edcaecf916aad7b7
JacobLai <jacle169@21cn.com>
2016-01-18 10:22:36 +0800

    json验证器

--- Q1ServerValidator/app.js
+++ Q1ServerValidator/app.js
@@ -1,12 +1,15 @@
-﻿var tv4 = require('tv4');
-var schema = {
-    "items": {
-        "type": "boolean"
-    }
+﻿var fs = require('fs');
+var tv4 = require('tv4');
+var pt = __dirname + "\\document.json";
+var schema = JSON.parse(fs.readFileSync(pt, 'utf8'));
+var data1 = {
+    users: [
+        { id: 1, username: "davidwalsh", numPosts: 404, realName: "David Walsh" },
+        { id: 2, username: "russianprince", numPosts: 12, realName: "Andrei Arshavin" }
+    ]
 };
-var data1 = [true, false];
 var data2 = [true, 123];
 
 console.log("data 1: " + tv4.validate(data1, schema)); // true
 console.log("data 2: " + tv4.validate(data2, schema)); // false
-console.log("data 2 error: " + JSON.stringify(tv4.error, null, 4));
+//console.log("data 2 error: " + JSON.stringify(tv4.error, null, 4));
