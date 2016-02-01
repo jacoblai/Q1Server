f53ab3523ad24bef04a580213432e814eb78b71c
JacobLai <jacle169@21cn.com>
2016-02-01 11:53:36 +0800

    删除没用项目

--- Q1ServerValidator/app.js
+++
@@ -1,9 +0,0 @@
-﻿var isvalid = require('isvalid');
-var schema = require('./schema.js');
-var inputData = { user: "jacob", pass: "pwd", localction1: [{ lat: '1.2255', lon: '128' }], ptime:'2012-03-15T16:13:14'};
-
-
-isvalid(inputData, schema , function (err, validData) {
-    console.log(JSON.stringify(validData));
-});
-
--- Q1ServerValidator/schema.js
+++
@@ -1,9 +0,0 @@
-﻿module.exports = {
-    type: Object,
-    schema: {
-        user: { type: String, required: true },
-        pass: { type: String, required: true },
-        localction: [{ lat: { type: Number }, lon: { type: Number } }],
-        ptime: { type: Date }
-    }
-};
